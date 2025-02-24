cd6df6c91db26947918cb265a6fc592631fc4c71
Risoras <gorgeicsongor@gmail.com>
2025-02-24 20:37:52 +0100

    tidied up the code

--- assignment1/korszam_assignment1.cpp
+++ assignment1/korszam_assignment1.cpp
@@ -15,31 +15,25 @@ int main(){
     return 0;
 }
 
-
 long double cosexp(long double x){
 
     return cos(x)*exp(-(x*x));
-
 }
 
 
 long double integral (long double ll, long double ul) {
 
-    long double I=0;
+    long double I=0;                            // result of integration
     long double x = ll;
     long double interval = abs(ul - ll);      // interval on which the function is integrated
     int N = round(interval/(1e-6));                // number of steps integration will take
-
     long double dx = interval/N;           // size of integration steps
-    long double thing =0;
 
     for (int i = 0; i < N; i++) {          // for loop that does the integration
-        thing = (x+x+dx)/2;
-        I += cosexp(thing)*dx;
+
+        I += (cosexp(x)+cosexp(x+dx))/2*dx;
         x += dx;
 
     }
     return I;
-
-
 }
