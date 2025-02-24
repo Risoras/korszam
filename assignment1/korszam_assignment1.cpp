23cffceb9418de516c3804d673b5bf836fa196eb
Risoras <gorgeicsongor@gmail.com>
2025-02-24 19:39:24 +0100

    first iteration, very simple, works somewhat

--- assignment1/korszam_assignment1.cpp
+++ assignment1/korszam_assignment1.cpp
@@ -2,8 +2,44 @@
 #include <iostream>
 using namespace std;
 
+long double cosexp(long double x);  // function to be integrated
+
+long double integral(long double ll, long double ul);  // function that will integrate
+
 int main(){
 
 
+    long double x=0;
+    long double result = integral(0,1);
+    cout << result <<endl;
     return 0;
+}
+
+
+long double cosexp(long double x){
+
+    return cos(x)*exp(-pow(x,2));
+
+}
+
+
+long double integral (long double ll, long double ul) {
+
+    long double I=0;
+    long double x = ll;
+    long double interval = abs(ul - ll);      // interval on which the function is integrated
+    long double N = round(interval/(1e-2));                // number of steps integration will take
+
+    long double dx = interval/N;           // size of integration steps
+
+
+    for (int i = 0; i < N; i++) {          // for loop that does the integration
+        I += cosexp(x)*dx;
+        x += x+dx;
+
+    }
+
+    return I;
+
+
 }
