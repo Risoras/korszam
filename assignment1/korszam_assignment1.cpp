07ff7dfeab3b62458386425d338d3250bba85096
Risoras <gorgeicsongor@gmail.com>
2025-03-01 23:05:24 +0100

    added some flexibility to the code

--- assignment1/korszam_assignment1.cpp
+++ assignment1/korszam_assignment1.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 long double cosexp(long double x);  // function to be integrated
 
-long double integral(long double ll, long double ul);  // function that will integrate
+long double integral(long double ll, long double ul, long double (*f)(long double a));  // function that will integrate
 
 int main(){
 
-
-    long double x=0;
-    long double result = integral(0,1);
+             // this will contain the result
+    long double ll = 0;           // lower boundary
+    long double ul = 1;           // upper boundary
+    long double result = integral(ll, ul, cosexp);
     cout << result <<endl;
     return 0;
 }
@@ -21,18 +22,19 @@ long double cosexp(long double x){
 }
 
 
-long double integral (long double ll, long double ul) {
+long double integral (long double ll, long double ul, long double (*f)(long double a)) {
 
-    long double I=0;                            // result of integration
+    long double I=0;
     long double interval = abs(ul - ll);      // interval on which the function is integrated
     int N = round(interval/(1e-6));                // number of steps integration will take
     long double dx = interval/N;           // size of integration steps
 
     for (int i = 0; i < N; i++) {          // for loop that does the integration
 
-        I += (cosexp(ll)+cosexp(ll+dx))/2*dx;
+        I += (f(ll)+f(ll+dx))/2*dx;
         ll += dx;
 
     }
+
     return I;
 }
